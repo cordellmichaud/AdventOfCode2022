0938244d713ba5321f02c7284bed687565aa0c95
Cordell Michaud <10409047+michaudcordell@users.noreply.github.com>
2022-12-06 20:32:06 -0700

    Complete day 6

---
+++ days/06/part2.py
@@ -0,0 +1,24 @@
+from collections import Counter, deque
+import pathlib
+import sys
+
+
+def main():
+    input_path: pathlib.Path = pathlib.Path(sys.argv[0]).parent / 'input.txt'
+    
+    chars_processed: int = 0
+    with input_path.open('r') as input_file:
+        char_window: deque[str] = deque()
+        for char_index, char in enumerate(input_file.read().strip()):
+            char_window.appendleft(char)
+            if char_index > 13:
+                char_window.pop()
+                if len(Counter(char_window)) == 14:
+                    chars_processed = char_index + 1
+                    break
+    
+    print('Characters processed before the first start-of-message marker is '
+          f'detected: {chars_processed}.')     
+ 
+if __name__ == '__main__':
+    main()
